d9f062e2a7794bde0f69fc0cccc40cc190bc0347
sema0205 <sazhinsema12@gmail.com>
2024-04-14 18:31:01 +0300

    edit migration file

--- db/migrations/000001_add_banner_tables.down.sql
+++ db/migrations/000001_add_banner_tables.down.sql
@@ -1,4 +1,5 @@
-DROP TABLE IF EXISTS banner;
-DROP TABLE IF EXISTS tag;
+DROP TABLE IF EXISTS banner_tag_item;
+DROP TABLE IF EXISTS banner_feature_item;
 DROP TABLE IF EXISTS feature;
-DROP TABLE IF EXISTS banner_feature_tag;
+DROP TABLE IF EXISTS tag;
+DROP TABLE IF EXISTS banner;
